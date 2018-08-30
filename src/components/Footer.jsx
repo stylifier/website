23696e8e2683fe909e5c93ca9648c5312764a43e
ali khoramshahi <alikhoramshahi@alis-MacBook.fritz.box>
2018-08-30 17:07:23 +0200

    add small changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -35,7 +35,17 @@ class Footer extends Component {
         |
         <a href="/policy" target="_blank" style={linkStyle}>Impressum</a>
         <br/>
-        <p style={{marginTop: 10, marginBottom: -10}}>
+          <a href="https://itunes.apple.com/us/app/stylifier/id1412217511?mt=8"
+            style={{
+              display: 'inline-block',
+              overflow: 'hidden',
+              background:'url(https://linkmaker.itunes.apple.com/assets/shared/badges/en-gb/appstore-lrg.svg) no-repeat',
+              width:135,
+              height:40,
+              margin: 20,
+              backgroundSize:'contain'
+            }}></a>
+        <p style={{marginBottom: -10}}>
           &copy; 2018 <a href="http://stylifier.com" target="_blank" style={{color: whiteBackground ? '': '#71d5de'}}>stylifier.com</a> | All Rights Reserved
         </p>
       </div>
